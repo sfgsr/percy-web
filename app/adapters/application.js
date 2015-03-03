d56248c91fba35f8ba08ea19a034abb104c72fa2
Mike Fotinakis <mike@fotinakis.com>
2015-03-03 14:00:28 -0800

    Remove initial prototype.

--- app/adapters/application.js
+++
@@ -1,7 +0,0 @@
-import DS from "ember-data";
-
-export default DS.ActiveModelAdapter.extend({
-  // TODO(fotinakis): #hardcoding.
-  // host: 'http://192.168.59.103:3000',
-  namespace: 'api',
-});
--- app/models/screenshot.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  repo: DS.belongsTo('repo', {async: true}),
-  image: DS.belongsTo('image'),
-  name: DS.attr(),
-});
