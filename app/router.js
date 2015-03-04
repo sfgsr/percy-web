7e9584ec01de1fe83761fe3f920ea1d767063cfa
Mike Fotinakis <mike@fotinakis.com>
2015-03-03 22:05:20 -0800

    Initial skeleton styling and basic pages.

--- app/router.js
+++ app/router.js
@@ -2,10 +2,16 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
 });
 
 Router.map(function() {
+  // this.route('login');
+  this.resource('repos', function() {
+    this.resource('repo', {path: '/:repo_id'}, function() {
+    });
+    this.route('foo');
+  });
 });
 
 export default Router;
