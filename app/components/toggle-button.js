b242dba5ffc5331dca9965811e6f038ce9522f13
Mike Fotinakis <mike@fotinakis.com>
2015-03-19 15:35:31 -0700

    Make header fixed, DRY it up, and fix toggle button state.

--- app/components/toggle-button.js
+++ app/components/toggle-button.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'button',
   classNames: ['Button', 'ToggleButton'],
-  classNameBindings: ['enabled::Button--primary'],
+  classNameBindings: ['enabled:Button--primary'],
   enabled: false,
   disabled: Ember.computed.not('enabled'),
   enabledText: 'Disable',
