bd869ac12aba4fc0ac02ac9292ac2cf97a369126
Mike Fotinakis <mike@fotinakis.com>
2015-03-27 11:56:38 -0700

    Use single find for repo model.

--- app/routes/repo.js
+++ app/routes/repo.js
@@ -2,20 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    // TODO(fotinakis): we need this crazy stuff because the API endpoint is the collection
-    // with filters, so multiple objects are returned but we want the model to be singular.
-    // Need to figure out a much better way to deal with URLs with slashes.
-    var self = this;
-    var promise = new Ember.RSVP.Promise(function(resolve, reject) {
-      var repoParams = {repo: params.repo_id, owner: self.modelFor('owner').id};
-      self.store.find('repo', repoParams).then(
-        function(repos) {
-          resolve(repos.content[0]);
-        },
-        function() {
-          reject.apply(this, arguments);
-        });
-    });
-    return promise;
+    return this.store.find('repo', this.modelFor('owner').id + '/' + params.repo_id);
   },
 });
