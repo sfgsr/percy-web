6ebd44dcf822e04d8c5d2f68e1ba0d93c86bea30
Mike Fotinakis <mike@fotinakis.com>
2015-04-16 14:51:39 -0700

    Build interactive overlay and move to two-column.

--- app/components/comparison-viewer.js
+++ app/components/comparison-viewer.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   comparison: null,
 
+  isOverlayEnabled: true,
   classNames: ['ComparisonViewer'],
+  actions: {
+    toggleOverlay: function() {
+      this.set('isOverlayEnabled', !this.get('isOverlayEnabled'));
+    },
+  },
 });
