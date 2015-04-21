01f67145fe712478a20c1c535fd91a17364f749d
Mike Fotinakis <mike@fotinakis.com>
2015-04-21 11:18:08 -0700

    Toggle showing no diff resources.

--- app/components/comparison-viewer.js
+++ app/components/comparison-viewer.js
@@ -3,11 +3,15 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   comparison: null,
 
+  showNoDiffResource: false,
   isOverlayEnabled: true,
   classNames: ['ComparisonViewer'],
   actions: {
     toggleOverlay: function() {
       this.set('isOverlayEnabled', !this.get('isOverlayEnabled'));
+    },
+    toggleNoDiffResource: function() {
+      this.set('showNoDiffResource', !this.get('showNoDiffResource'));
     },
   },
 });
