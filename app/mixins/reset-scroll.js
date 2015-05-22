32b717f564b11028399ce9abb41fe54cdbc504da
Mike Fotinakis <mike@fotinakis.com>
2015-05-21 22:05:19 -0700

    Add privacy policy and terms of service, plus small css layout refactor.

---
+++ app/mixins/reset-scroll.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+var ResetScrollMixin = Ember.Mixin.create({
+  activate: function() {
+    this._super();
+    window.scrollTo(0, 0);
+  },
+});
+
+export default ResetScrollMixin;
