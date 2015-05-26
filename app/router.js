dd6147657ace9820ce89ccd73abbc04d36a20822
Mike Fotinakis <mike@fotinakis.com>
2015-05-26 09:13:54 -0700

    Add google analytics.

--- app/router.js
+++ app/router.js
@@ -3,6 +3,13 @@ import config from './config/environment';
 
 var Router = Ember.Router.extend({
   location: config.locationType,
+
+  notifyGoogleAnalytics: function() {
+    return window.ga('send', 'pageview', {
+      'page': this.get('url'),
+      'title': this.get('url')
+    });
+  }.on('didTransition'),
 });
 
 Router.map(function() {
