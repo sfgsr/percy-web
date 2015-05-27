30202950e6d948af13556f9227a3d0a166df7a73
Mike Fotinakis <mike@fotinakis.com>
2015-05-27 11:17:50 -0700

    Only show login-extended-button on current user owner.

--- app/components/repo-list.js
+++ app/components/repo-list.js
@@ -5,6 +5,11 @@ export default Ember.Component.extend({
   filterByOwner: null,
   classes: null,
 
+  onCurrentUserOwner: function() {
+    // TODO: why can't we use Ember.computed.equal with these?
+    return this.get('session.secure.user.login') === this.get('filterByOwner.login');
+  }.property('session.secure.user', 'filterByOwner'),
+
   filteredRepos: Ember.computed.filter('repos', function(repo) {
     return repo.get('ownerLogin') === this.get('filterByOwner.login');
   }).property('repos', 'filterByOwner'),
