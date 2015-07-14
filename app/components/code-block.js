1cb766133836e8d6ffb845f8898952e640c59205
Mike Fotinakis <mike@fotinakis.com>
2015-07-14 14:51:09 -0700

    Update docs per discussion with customer.

--- app/components/code-block.js
+++ app/components/code-block.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   lang: null,
 
+  classNames: ['CodeBlock'],
   highlight: function() {
     this.$('pre code').each(function(i, block) {
       window.hljs.highlightBlock(block);
