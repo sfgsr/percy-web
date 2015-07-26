1409a71564d001c510cb1b5b16bb110269a584b7
Mike Fotinakis <mike@fotinakis.com>
2015-07-26 16:33:56 -0700

    Enforce 2 private repo limit.

--- app/routes/application.js
+++ app/routes/application.js
@@ -18,7 +18,20 @@ export default Ember.Route.extend(ApplicationRouteMixin, {
       this.send('disablingRepo', repo.disable(), repo);
     },
     enableRepo: function(repo) {
-      this.send('enablingRepo', repo.enable(), repo);
+      var self = this;
+      var promise = repo.enable();
+      promise.then(null, function(response) {
+        if (response.status === 402) {
+          var msg = [
+            "Whoops! You've hit the limit of 2 private repos on the free plan.",
+            "\n\nUpgrading is easy, would you like to see the available plans?",
+          ].join('');
+          if (confirm(msg)) {
+            self.transitionTo('account');
+          }
+        }
+      });
+      this.send('enablingRepo', promise, repo);
     },
   },
 });
