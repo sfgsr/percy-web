bad0198a3d832295ac9ed976817e70169cf1618e
Mike Fotinakis <mike@fotinakis.com>
2015-08-26 18:35:32 -0700

    Checkpoint improvements to build state UI.

---
+++ app/components/build-state.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  build: null,
+  classes: null,
+
+  classNames: ['BuildState'],
+  classNameBindings: [
+    'classes',
+
+    'build.isPending:BuildState--pending',
+    'build.isProcessing:BuildState--processing',
+    'build.isFinished:BuildState--finished',
+    'build.isFailed:BuildState--failed',
+
+    'build.isApproved:BuildState--approved',
+    'build.hasDiffs:BuildState--hasDiffs',
+  ],
+});
--- app/models/snapshot.js
+++ app/models/snapshot.js
@@ -4,7 +4,14 @@ export default DS.Model.extend({
   name: DS.attr(),
   build: DS.belongsTo('build'),
   screenshots: DS.hasMany('screenshot'),
+  startedProcessingAt: DS.attr('date'),
   finishedProcessingAt: DS.attr('date'),
+  processingDurationSeconds: function() {
+    var finished = this.get('finishedProcessingAt');
+    var started = this.get('startedProcessingAt');
+    var milliseconds = window.moment(finished).diff(started);
+    return milliseconds / 1000;
+  }.property('startedProcessingAt', 'finishedProcessingAt'),
   createdAt: DS.attr('date'),
   updatedAt: DS.attr('date'),
 });
