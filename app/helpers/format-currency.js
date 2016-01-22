f1cfaf95111ce20a9ef0f08baaba615c310b6d71
Mike Fotinakis <mike@fotinakis.com>
2016-01-22 15:48:49 -0800

    Checkpoint many upgrade changes.

--- app/helpers/format-currency.js
+++ app/helpers/format-currency.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import Formatting from '../lib/formatting';
 
-export default Ember.Handlebars.makeBoundHelper(function(value, options) {
+export default Ember.Helper.helper(function(value, options) {
   return Formatting.formatCurrency(value, options.hash);
 });
