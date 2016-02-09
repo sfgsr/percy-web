7aebd3914514364c3ac90e4164bb9e847f7624ad
Mike Fotinakis <mike@fotinakis.com>
2016-02-08 16:11:37 -0800

    Add fix for directly visiting login route.

--- app/routes/login.js
+++ app/routes/login.js
@@ -2,13 +2,20 @@ import Ember from 'ember';
 import utils from '../lib/utils';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
+// TODO: fix broken login route when user is already logged in.
+
 export default Ember.Route.extend(UnauthenticatedRouteMixin, {
   session: Ember.inject.service(),
 
   afterModel: function() {
     // Right now location.href is the URL before the transition to the login route has completed.
     // Make sure to pass redirectTo into the authenticator so that we come back to that URL.
-    let options = {redirectTo: window.location.href};
+    let options = {};
+    if (!window.location.href.endsWith('/login')) {
+      options['redirectTo'] = window.location.href;
+    } else {
+      options['redirectTo'] = '/';
+    }
     this.get('session').authenticate('authenticator:custom', options).then(
       function() {
         var finalRedirect = utils.getQueryParam('redirect_to');
