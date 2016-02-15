ab4f6cfaf6a6f56a3ab4c7a050f73e84b18c6d95
Mike Fotinakis <mike@fotinakis.com>
2016-02-15 14:56:09 -0800

    Fix build warning and add footer to pricing page.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
-import utils from '../lib/utils';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
   session: Ember.inject.service(),
