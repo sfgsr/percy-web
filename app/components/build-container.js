d182785325ad0eac2eb29b6ab98e35a4ec7be70f
Mike Fotinakis <mike@fotinakis.com>
2016-02-29 19:08:39 -0800

    Checkpoint revamp build-state to display more info and pluralize correctly.

--- app/components/build-container.js
+++ app/components/build-container.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   build: null,
   classNames: ['BuildContainer'],
-  selectedWidths: [1024],
+  selectedWidths: [1280],
   actions: {
     updateSelectedWidths: function(widths) {
       this.set('selectedWidths', widths);
