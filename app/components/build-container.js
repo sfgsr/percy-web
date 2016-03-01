fb2dc635cdda6aa666c2e0c1fbce958285990091
Mike Fotinakis <mike@fotinakis.com>
2016-02-29 16:29:57 -0800

    Some responsive cleanup.

--- app/components/build-container.js
+++ app/components/build-container.js
@@ -9,16 +9,4 @@ export default Ember.Component.extend({
       this.set('selectedWidths', widths);
     },
   },
-
-  // defaultWidth: function() {
-  //   return Math.max(this.get('build.comparisonWidths'));
-  // }.property('build.comparisionWidths'),
-  // init: function() {
-  //   this._super(...arguments);
-
-  //   // TODO: this is crap and doesn't get
-  //   this.get('selectedWidths').push(this.get('defaultWidth'));
-  //   this.send('updateSelectedWidths', this.get('defaultWidth'));
-  // },
-
 });
