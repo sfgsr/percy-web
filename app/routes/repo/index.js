3797778c328eba3b016160cd5199a031e3dfd1d3
Mike Fotinakis <mike@fotinakis.com>
2016-03-02 16:38:41 -0800

    More style updates and change behavior of builds loading and refreshing.

--- app/routes/repo/index.js
+++ app/routes/repo/index.js
@@ -4,4 +4,15 @@ export default Ember.Route.extend({
   model: function() {
     return this.modelFor('repo');
   },
+  afterModel: function(repo) {
+    repo.get('builds'); // Load builds async, not returned and blocking.
+  },
+  actions: {
+    refresh: function() {
+      let repo = this.modelFor('repo.index')
+      repo.reload().then(function(repo) {
+        repo.get('builds').reload();
+      });
+    },
+  },
 });
