a7b06ae7fa68b44754fdcab1e05be66cfca83bcd
Mike Fotinakis <mike@fotinakis.com>
2016-03-25 19:08:49 -0700

    Checkpoint crazy percy test snapshoting integration.

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -12,6 +12,8 @@ import Ember from 'ember';
 describe('Acceptance: Homepage', function() {
   let application;
 
+  this.timeout(20000);
+
   beforeEach(function() {
     application = startApp();
   });
@@ -25,6 +27,34 @@ describe('Acceptance: Homepage', function() {
 
     andThen(function() {
       expect(currentPath()).to.equal('index');
+    });
+    andThen(function() {
+      let doctypeNode = document.doctype;
+      let doctype = "<!DOCTYPE "
+        + doctypeNode.name
+        + (doctypeNode.publicId ? ' PUBLIC "' + doctypeNode.publicId + '"' : '')
+        + (!doctypeNode.publicId && doctypeNode.systemId ? ' SYSTEM' : '')
+        + (doctypeNode.systemId ? ' "' + doctypeNode.systemId + '"' : '')
+        + '>';
+
+      Ember.$('#ember-testing-container').css('height', '100%');
+      Ember.$('#ember-testing-container').css('width', '100%');
+      Ember.$('#ember-testing').css('zoom', '100%');
+
+      let html = Ember.$('html')[0].outerHTML;
+
+      Ember.$.ajax('/_percy/snapshot', {
+        method: 'POST',
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify({
+          name: 'homepage stuff (foo)',
+          content: doctype + html,
+        }),
+      }).then(function() {
+        Ember.$.ajax('/_percy/finalize_build', {
+          method: 'POST',
+        });
+      });
     });
   });
 });
