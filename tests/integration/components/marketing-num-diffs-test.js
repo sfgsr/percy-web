9e19421793960c0f24502bf2e94c1d23b3ab5ffd
Mike Fotinakis <mike@fotinakis.com>
2016-06-13 12:16:53 -0700

    Add some Percy component tests.

---
+++ tests/integration/components/marketing-num-diffs-test.js
@@ -0,0 +1,22 @@
+/* jshint expr:true */
+import { expect } from 'chai';
+import {
+  describeComponent,
+  it
+} from 'ember-mocha';
+import hbs from 'htmlbars-inline-precompile';
+import { percySnapshot } from 'ember-percy';
+
+describeComponent(
+  'marketing-num-diffs',
+  'Integration: MarketingNumDiffsComponent',
+  {
+    integration: true
+  },
+  function() {
+    it('renders', function() {
+      this.render(hbs`{{marketing-num-diffs}}`);
+      percySnapshot(this.test.fullTitle(), this.$());
+    });
+  }
+);
---
+++ tests/integration/components/meter-bar-test.js
@@ -0,0 +1,30 @@
+/* jshint expr:true */
+import { expect } from 'chai';
+import {
+  describeComponent,
+  it
+} from 'ember-mocha';
+import hbs from 'htmlbars-inline-precompile';
+import { percySnapshot } from 'ember-percy';
+
+
+describeComponent(
+  'meter-bar',
+  'Integration: MeterBarComponent',
+  {
+    integration: true
+  },
+  function() {
+    it('renders', function() {
+      this.set('count', 0);
+      this.render(hbs`{{meter-bar count=count total=100}}`);
+      percySnapshot('meter bar zero');
+
+      this.set('count', 50);
+      percySnapshot('meter bar half');
+
+      this.set('count', 100);
+      percySnapshot('meter bar full');
+    });
+  }
+);
