d6ce4f2c8c2542bca5ff3a3fac5e7afb0f42c32c
Mike Fotinakis <mike@fotinakis.com>
2016-07-01 16:11:49 -0700

    Add highlightjs from bower.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -16,8 +16,8 @@ module.exports = function(defaults) {
 
   app.import('bower_components/accounting.js/accounting.js');
   app.import('bower_components/hint.css/hint.css');
-  app.import('vendor/highlight/styles/github.css');
-  app.import('vendor/highlight/highlight.pack.js');
+  app.import('bower_components/highlightjs/styles/github.css');
+  app.import('bower_components/highlightjs/highlight.pack.js');
 
   return app.toTree();
 };
