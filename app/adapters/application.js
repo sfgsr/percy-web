654439dcdb6505e74af261baf72362fe70140c45
Mike Fotinakis <mike@fotinakis.com>
2016-10-12 11:15:22 -0700

    Fix jshint.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.JSONAPIAdapter.extend({
   namespace: 'api/v1',
 
-  isInvalid: function(status, headers, payload) {
+  isInvalid: function(status) {
     // NOTE: right now, the Percy API uses HTTP 400 when it should use HTTP 422 in many cases.
     // For that reason, we need to add 400 to the isInvalid check so that model.errors is populated
     // correctly.
