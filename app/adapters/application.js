c5cb707bb09a093a8de1cd953578addafa25fdc1
Mike Fotinakis <mike@fotinakis.com>
2016-10-11 18:26:59 -0700

    Add isInvalid customization to fix error model handling.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,13 @@ import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
   namespace: 'api/v1',
+
+  isInvalid: function(status, headers, payload) {
+    // NOTE: right now, the Percy API uses HTTP 400 when it should use HTTP 422 in many cases.
+    // For that reason, we need to add 400 to the isInvalid check so that model.errors is populated
+    // correctly.
+    // TODO: when the API is changed to be more correct, we should drop this method.
+    return status === 422 || status === 400;
+  },
 });
 
