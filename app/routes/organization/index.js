34ecaa86cc7590facdc7aa8f3318ec634ed9330a
Mike Fotinakis <mike@fotinakis.com>
2016-10-26 09:42:23 -0700

    Checkpoint work.

--- app/routes/organization/index.js
+++ app/routes/organization/index.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  redirect() {
+    this.send('redirectToDefaultOrganization');
+  },
   afterModel(model) {
     model.get('projects').reload();
   },
