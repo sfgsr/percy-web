0a61066134c2bd2dcaa5ed295fb93d8eddacfeb9
Mike Fotinakis <mike@fotinakis.com>
2016-10-26 14:33:20 -0700

    Upgrade docs and initial project page.

--- app/models/project.js
+++ app/models/project.js
@@ -11,6 +11,12 @@ export default DS.Model.extend({
   createdAt: DS.attr('date'),
   updatedAt: DS.attr('date'),
 
+  // Repo will be set if this project is linked to a GitHub repository.
   repo: DS.belongsTo('repo', {async: false}),
+
   builds: DS.hasMany('build', {async: true}),
+
+  fullSlug: Ember.computed('organization', function() {
+    return `${this.get('organization.slug')}/${this.get('slug')}`;
+  }),
 });
