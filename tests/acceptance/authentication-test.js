cd8b9c32e465420a5daea676af75514b3cd58155
Miklós Fazekas <mfazekas@szemafor.com>
2016-11-22 23:21:01 +0100

    Added test for expired invitation code (#37)

--- tests/acceptance/authentication-test.js
+++ tests/acceptance/authentication-test.js
@@ -41,13 +41,22 @@ describe('Acceptance: Join', function() {
       let organization = server.create('organization', {name: 'Test org'});
       server.create('organizationUser', {user: user, organization: organization});
       let anotherOrganization = server.create('organization', {name: 'Other org'});
-      server.create('invite', {id: 'valid_code', organization: anotherOrganization});
+      server.create('invite', {id: 'valid-code', organization: anotherOrganization});
+      server.create('invite', {id: 'expired-code', isExpired: true});
       return {user: user};
+    });
+
+    it('expired rejected', function () {
+      visit('/join/expired-code');
+      andThen(() => {
+        expect(currentPath()).to.equal('join');
+      });
+      return percySnapshot(this.test.fullTitle());
     });
 
     it('invalid rejected', function() {
       renderAdapterErrorsAsPage(() => {
-        visit('/join/invalid_code');
+        visit('/join/invalid-code');
         andThen(() => {
           expect(currentPath()).to.equal('error');
         });
@@ -56,7 +65,7 @@ describe('Acceptance: Join', function() {
     });
 
     it('valid accepted', function() {
-      visit('/join/valid_code');
+      visit('/join/valid-code');
       andThen(() => {
         expect(currentPath()).to.equal('join');
       });
