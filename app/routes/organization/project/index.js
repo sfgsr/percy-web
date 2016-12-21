78cd69ea8f09a8f6d830d8985220b8df359c8ef1
Mike Fotinakis <mike@fotinakis.com>
2016-12-21 11:18:04 -0800

    Reset scroll on project index and delete some dead code. (#64)

--- app/routes/organization/project/index.js
+++ app/routes/organization/project/index.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import ResetScrollMixin from 'percy-web/mixins/reset-scroll';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+export default Ember.Route.extend(AuthenticatedRouteMixin, ResetScrollMixin, {
 });
 
