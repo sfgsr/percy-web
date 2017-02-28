f008a14e8f2b10db9c77d6e4158bd36a49805561
Tim Haines <tmhaines@gmail.com>
2017-02-28 11:30:58 -0800

    Ping intercom with the company id (#81)
    Ping intercom with the company id when in an organization route

--- app/routes/organization.js
+++ app/routes/organization.js
@@ -2,11 +2,23 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  currentUser: Ember.computed.alias('session.data.authenticated.user'),
+
   afterModel(model) {
     try {
       localStorage.setItem('lastOrganizationSlug', model.get('slug'));
     } catch (_) {
       // Safari throws errors while accessing localStorage in private mode.
+    }
+
+    let userId = this.get('currentUser.id');
+    if (userId && window.Intercom) {
+      window.Intercom('update', {
+        user_id: userId,
+        company: {
+          id: model.get('id'),
+        },
+      });
     }
   },
 });
