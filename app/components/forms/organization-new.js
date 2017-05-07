4ee0d0ca6a9b834340fcad556286df5e40980c91
Mike Fotinakis <mike@fotinakis.com>
2017-05-07 13:37:33 -0700

    Checkpoint initial work on external billing provider support.

--- app/components/forms/organization-new.js
+++ app/components/forms/organization-new.js
@@ -3,13 +3,31 @@ import BaseFormComponent from './base';
 import OrganizationNewValidations from '../../validations/organization-new';
 
 export default BaseFormComponent.extend({
+  marketplaceListingPlanId: null,
   classes: null,
 
   classNames: ['FormsOrganizationNew', 'Form'],
   classNameBindings: ['classes'],
 
+  // Setup data for creating an org from different billing providers and marketplaces.
+  _billingProvider: Ember.computed('marketplaceListingPlanId', function() {
+    let marketplaceListingPlanId = this.get('marketplaceListingPlanId');
+    if (marketplaceListingPlanId) {
+      return 'ghm';
+    }
+  }),
+  _billingProviderData: Ember.computed('marketplaceListingPlanId', function() {
+    let marketplaceListingPlanId = this.get('marketplaceListingPlanId');
+    if (marketplaceListingPlanId) {
+      return JSON.stringify({marketplace_listing_plan_id: parseInt(marketplaceListingPlanId)});
+    }
+  }),
+
   model: Ember.computed(function() {
-    return this.get('store').createRecord('organization', {});
+    return this.get('store').createRecord('organization', {
+      billingProvider: this.get('_billingProvider'),
+      billingProviderData: this.get('_billingProviderData'),
+    });
   }),
   validator: OrganizationNewValidations,
 });
