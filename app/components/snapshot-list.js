c131d27a250b16dc6eb3f08ef21228ee4d80041a
Tim Haines <tmhaines@gmail.com>
2017-12-06 16:26:11 -0800

    Fix snapshot linking  (#302)
    * Prioritize snapshots with a above those with no diffs
    
    * Update maxDiffRatioAnyWidth for readability

--- app/components/snapshot-list.js
+++ app/components/snapshot-list.js
@@ -26,10 +26,25 @@ export default Component.extend({
     }
 
     return snapshots.sort(function(a, b) {
+      // Prioritize snapshots with diffs at any widths over snapshots with no diffs at any widths
+      function maxDiffRatioAnyWidth(comparisons) {
+        let comparisonWidths = comparisons.mapBy('smartDiffRatio').filter(x => x);
+        return Math.max(0, ...comparisonWidths); // Provide minimum of one param to avoid -Infinity
+      }
+
+      let maxComparisonDiffA = maxDiffRatioAnyWidth(a.get('comparisons'));
+      let maxComparisonDiffB = maxDiffRatioAnyWidth(b.get('comparisons'));
+      if (maxComparisonDiffA > 0 && maxComparisonDiffB == 0) {
+        // First snapshot has diffs, second doesn't at any widths
+        return -1;
+      } else if (maxComparisonDiffA == 0 && maxComparisonDiffB > 0) {
+        // Second snapshot has diffs, first doesn't at any widths
+        return 1;
+      }
+
+      // Next prioritize snapshots with comparisons at the current width
       let comparisonForA = comparisonAtCurrentWidth(a);
       let comparisonForB = comparisonAtCurrentWidth(b);
-
-      // First-level sort: priortize snapshots with comparisons at the current width to the top.
       if (comparisonForA && !comparisonForB) {
         // First snapshot has a comparison at the current width.
         return -1;
@@ -41,13 +56,7 @@ export default Component.extend({
         return comparisonForB.get('smartDiffRatio') - comparisonForA.get('smartDiffRatio');
       }
 
-      // Second sort: by diff ratio across all widths.
-      function maxDiffRatioAnyWidth(comparisons) {
-        return Math.max.apply(null, comparisons.mapBy('smartDiffRatio').filter(x => x));
-      }
-      let maxComparisonDiffA = maxDiffRatioAnyWidth(a.get('comparisons'));
-      let maxComparisonDiffB = maxDiffRatioAnyWidth(b.get('comparisons'));
-
+      // Finally, sort by diff ratio across all widths.
       // Sorts descending.
       return maxComparisonDiffB + maxComparisonDiffA;
     });
