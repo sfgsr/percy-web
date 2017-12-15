8dfc8a120a774e44012553b1c68d173350aa57e4
LC <lrc3233@gmail.com>
2017-12-15 12:54:35 -0800

    improve test snapshot titles

--- tests/acceptance/profile-test.js
+++ tests/acceptance/profile-test.js
@@ -12,18 +12,18 @@ describe('Acceptance: Profile', function() {
   it('displays profile info page', function() {
     ProfilePageObject.visitInfoPage();
 
-    percySnapshot(this.test.fullTitle() + 'before submitting');
+    percySnapshot(this.test.fullTitle() + ' before submitting');
 
     ProfilePageObject.fillInName('Tyrion Targaryen')
       .fillInEmail('tyrion@motherofdragons.biz')
       .submitInfoForm();
 
-    percySnapshot(this.test.fullTitle() + 'after submitting');
+    percySnapshot(this.test.fullTitle() + ' after submitting');
   });
 
   it('displays connected-accounts info page', function() {
     ProfilePageObject.visitConnectedAccountsPage();
 
-    percySnapshot(this.test.fullTitle() + 'without github integration');
+    percySnapshot(this.test.fullTitle() + ' with github integration');
   });
 });
