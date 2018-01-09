7d837a5617fd82e240dc1d57193e956dadfbe6c4
LC <lrc3233@gmail.com>
2018-01-08 18:16:00 -0800

    maybe?

--- .buildkite/parse-xml.js
+++ .buildkite/parse-xml.js
@@ -1,41 +1,28 @@
-  // var fs = require('fs');
-  // var path = require('path');
-
-  // // Buffer mydata
-  // var BUFFER = bufferFile('../junit/test-results.xml');
-
-  // function bufferFile(relPath) {
-  //   console.log( fs.readFileSync(path.join(__dirname, relPath), { encoding: 'utf8' })); // zzzz....
-  // }
-
-
 var fs = require('fs');
-var $ = require('jquery');
-var __dirname = path.resolve();
-var relPath = 'junit/test-results.xml';
-var path = path.join(__dirname, relPath);
-var file = fs.readFileSync(path, "utf8");
+var path = require('path');
+var parseString = require('xml2js').parseString;
+var relPath = '../junit/test-results.xml';
+var p = path.join(__dirname, relPath);
+var file = fs.readFileSync(p, 'utf8');
 var xml = file;
 
-var parseString = require('xml2js').parseString;
-var res;
-parseString(xml, function(err, result){
-  res = result;
-  // console.dir(result);
-})
+var parsedXml;
+parseString(xml, function(err, result) {
+  parsedXml = result;
+});
 
-console.log(res)
-var failures = res.testsuite.testcase.filter((test) => {
+var failures = parsedXml.testsuite.testcase.filter(test => {
   return !!test.error;
 });
 
-
-var html = `There were ${res.testsuite.$.failures} out of ${res.testsuite.$.tests} tests.`
+var markdown = `There were ${parsedXml.testsuite.$.failures} out of ${parsedXml.testsuite.$.tests} tests.`;
 if (failures.length) {
-  html += '<ul>'
-  failures.forEach((failure) => {
-    html += `<div>${failure.$.name}</div>`;
-    html += `<div>${failure.error[0].$.message}</div>`;
+  failures.forEach(failure => {
+    markdown += '<details>';
+    markdown += `<summary><code>${failure.$.name}</code></summary>\n`;
+    markdown += `<code>${failure.error[0].$.message}</code>\n\n\n`;
+    markdown += '</details>';
   });
-  html += '</ul>'
 }
+
+process.stdout.write(markdown);
