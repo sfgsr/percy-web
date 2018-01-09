708e069719d14898d14c706efee0ef49cdb651c4
LC <lrc3233@gmail.com>
2018-01-08 16:53:56 -0800

    wip2

--- .buildkite/parse-xml.js
+++ .buildkite/parse-xml.js
@@ -15,4 +15,27 @@ var __dirname = path.resolve();
 var relPath = 'junit/test-results.xml';
 var path = path.join(__dirname, relPath);
 var file = fs.readFileSync(path, "utf8");
-console.log(file);
+var xml = file;
+
+var parseString = require('xml2js').parseString;
+var res;
+parseString(xml, function(err, result){
+  res = result;
+  // console.dir(result);
+})
+
+console.log(res)
+var failures = res.testsuite.testcase.filter((test) => {
+  return !!test.error;
+});
+
+
+var html = `There were ${res.testsuite.$.failures} out of ${res.testsuite.$.tests} tests.`
+if (failures.length) {
+  html += '<ul>'
+  failures.forEach((failure) => {
+    html += `<div>${failure.$.name}</div>`;
+    html += `<div>${failure.error[0].$.message}</div>`;
+  });
+  html += '</ul>'
+}
