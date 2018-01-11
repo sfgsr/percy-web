c95aff58666a77c84df93646a4592e87c5cf289a
Faun <github@faun.me>
2018-01-10 17:18:24 -0800

    Add additional flash message for duplicate email (#355)

--- app/routes/auth-failure.js
+++ app/routes/auth-failure.js
@@ -1,13 +1,23 @@
 import Route from '@ember/routing/route';
 import {inject as service} from '@ember/service';
 
+import utils from 'percy-web/lib/utils';
+
 export default Route.extend({
   flashMessages: service(),
   beforeModel() {
-    this.get('flashMessages').danger(
-      'There was a problem with logging in. \
-        Please try again or contact us if the issue does not resolve.',
-    );
+    let message = utils.getQueryParam('message');
+    if (message === 'duplicate_email') {
+      this.get('flashMessages').danger(
+        'There was a problem with logging in. \
+          The email you provided already exists.',
+      );
+    } else {
+      this.get('flashMessages').danger(
+        'There was a problem with logging in. \
+          Please try again or contact us if the issue does not resolve.',
+      );
+    }
 
     this.transitionTo('index');
   },
