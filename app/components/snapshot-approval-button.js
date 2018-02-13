370d23b37f455b32032eaca275bd881f2a7a17dd
Lindsay <lrc3233@gmail.com>
2018-02-12 17:54:51 -0800

    temporarily assign snapshot properties to prevent UI jank (#416)

--- app/components/snapshot-approval-button.js
+++ app/components/snapshot-approval-button.js
@@ -9,9 +9,18 @@ export default Component.extend({
   actions: {
     approveSnapshot() {
       this.set('isLoading', true);
-      this.createReview([this.get('snapshot')]).finally(() => {
-        this.set('isLoading', false);
-      });
+      this.createReview([this.get('snapshot')])
+        .then(() => {
+          // The time between when we get the response back from the server and the time
+          // it takes Ember to process and render the returned data is actually quite long (~1s)
+          // on production. So setting these properties here disguises that transition time and
+          // prevents jank in the Approve/Approved button state
+          this.set('snapshot.reviewState', 'approved');
+          this.set('snapshot.reviewStateReason', 'user_approved');
+        })
+        .finally(() => {
+          this.set('isLoading', false);
+        });
     },
   },
 });
