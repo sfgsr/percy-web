e521b5702852b06471c50c4100f91b3281046507
LC <lrc3233@gmail.com>
2018-02-15 17:48:35 -0800

    dont show snapshot list until build is finished, i.e. until all comparisons are finished

---
+++ tests/integration/components/build-container-test.js
@@ -0,0 +1,54 @@
+import {setupComponentTest} from 'ember-mocha';
+import {expect} from 'chai';
+import {it, describe, beforeEach} from 'mocha';
+import {percySnapshot} from 'ember-percy';
+import hbs from 'htmlbars-inline-precompile';
+import {make, makeList, manualSetup} from 'ember-data-factory-guy';
+import sinon from 'sinon';
+import BuildPage from 'percy-web/tests/pages/build-page';
+
+describe('Integration: BuildContainer', function() {
+  setupComponentTest('build-container', {
+    integration: true,
+  });
+
+  beforeEach(function() {
+    manualSetup(this.container);
+    BuildPage.setContext(this);
+  });
+
+  it('does not display snapshots while build is processing', function() {
+    const build = make('build', 'processing');
+    const snapshots = makeList('snapshot', ['withComparisons', 'withNoDiffs'], {build});
+    const stub = sinon.stub();
+    this.setProperties({build, snapshots, stub});
+
+    this.render(hbs`{{build-container
+      build=build
+      snapshots=snapshots
+      createReview=stub
+    }}`);
+
+    percySnapshot(this.test.fullTitle());
+    expect(BuildPage.snapshotList.isVisible).to.equal(false);
+  });
+
+  it('displays snapshots when build is finished', function() {
+    const build = make('build', 'finished');
+    const diffSnapshot = make('snapshot', 'withComparisons', {build});
+    const sameSnapshot = make('snapshot', 'withNoDiffs', {build});
+    const stub = sinon.stub();
+    this.setProperties({build, stub, snapshots: [sameSnapshot, diffSnapshot]});
+
+    this.render(hbs`{{build-container
+      build=build
+      snapshots=snapshots
+      createReview=stub
+    }}`);
+    percySnapshot(this.test.fullTitle());
+
+    expect(BuildPage.snapshotList.isVisible).to.equal(true);
+    expect(BuildPage.snapshotList.snapshots().count).to.equal(1);
+    expect(BuildPage.snapshotList.isNoDiffsBatchVisible).to.equal(true);
+  });
+});
