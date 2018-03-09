e41bf2fe50a1ca78e8cecf75c8314723038ccd44
Lindsay <lrc3233@gmail.com>
2018-03-09 11:02:56 -0800

    default docs route (index) scrolls to top (#463)

--- app/routes/docs/index.js
+++ app/routes/docs/index.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
+import ResetScrollMixin from 'percy-web/mixins/reset-scroll';
 
-export default Route.extend({
+export default Route.extend(ResetScrollMixin, {
   actions: {
     didTransition() {
       this._super(...arguments);
