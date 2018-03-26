1546c59906d67a0a7274f07e3078e61332bccc90
Lindsay <lrc3233@gmail.com>
2018-03-26 11:21:29 -0700

    ignore errors with TransitionAborted (#482)

--- app/initializers/sentry.js
+++ app/initializers/sentry.js
@@ -1,7 +1,10 @@
 import config from 'percy-web/config/environment';
 
 export function initialize(/* application */) {
-  Raven.config(config.APP.SENTRY_URL, {release: config.APP.VERSION})
+  Raven.config(config.APP.SENTRY_URL, {
+    release: config.APP.VERSION,
+    ignoreErrors: ['TransitionAborted'],
+  })
     .addPlugin(Raven.Plugins.Ember)
     .install();
 }
